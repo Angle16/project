43b70b2e17611b6afd7be373148efd1bced35a8d
Your Name <you@example.com>
2018-07-01 10:25:18 +0800

    shan

--- src/App.js
+++ src/App.js
@@ -16,20 +16,13 @@ import BScreen from './Screen/BScreen';
 class App extends Component {
   render() {
     return (
-      <DocumentTitle title='HomeTitle'>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-          <Button>Click</Button>
+    
+        <div>
+         
           <Route path={'/a'} component={AScreen} />
           <Route path={'/b'} component={BScreen} />
         </div>
-      </DocumentTitle>
+      
     );
   }
 }
